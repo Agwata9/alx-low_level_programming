6f0f4753fcae614d68fff1ef7f83d248d6a91e21
Agwata Geoffrey <agwata9@gmail.com>
2022-06-02 08:34:23 -0700

    assignment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-* Main programming is like building a multilingual puzzle
+* main - Write '"Programming is like building a multilingual puzzle'
 * Return 0 always (success)
 */
 int main(void)
