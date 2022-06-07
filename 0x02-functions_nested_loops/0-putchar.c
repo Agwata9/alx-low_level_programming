f77ab61937248cd401627510282be072c822d91c
Agwata Geoffrey <agwata9@gmail.com>
2022-06-07 02:19:48 -0700

    assignment 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*
+* main - program that prints "_putchar" from a character array
+**
+* Returns : 0 on (success)
+*/
+int main(void)
+
+{
+	char c[] = "_putchar";
+	int i;
+
+	for (i = 0 ; i < 8 ; i++)
+	{
+		putchar(c[i]);
+	}
+	return (0);
+}
