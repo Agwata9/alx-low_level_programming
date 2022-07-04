e3ef4fa42ee47626695f174858373d5c4f19849b
Agwata9 <agwata9@gmail.com>
2022-07-04 09:53:06 -0700

    submission

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef _0-object_like_macro_h_
-#define _0-object_like_macro_h_
+#ifndef _object_like_macro_h_
+#define _object_like_macro_h_
 
 #define SIZE 1024
 
-#endif /* _0-object_like_macro_h_*/
+#endif /* _0-object_like_macro_h_ */
--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,5 @@
-#ifndef _3-function_like_macro_h_
-#define _3-function_like_macro_h_
+#ifndef _function_like_macro_h_
+#define _function_like_macro_h_
 
 #define ABS(x) (((x) < (0)) ? ((x) * (-1)) : (x))
 
