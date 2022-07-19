7d5763627680e0f05901f15dca0dd39e526b37eb
Agwata9 <agwata9@gmail.com>
2022-07-19 12:43:43 -0700

    submission

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,21 +1,21 @@
-#include <stdio.h>
+
 #include "lists.h"
+
 /**
- * listint_len - get len of list
- * Return: size_t
- * @h: list
+ * listint_len - returns the # of elements in list
+ * @h: points to start of list
+ * Return: # of elements in size_t form
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t len =0;
-	
+	size_t nodes = 0;
+
 	if (!h)
 		return (0);
-
 	while (h)
 	{
-		len++;
+		nodes += 1;
 		h = h->next;
 	}
-	return (len);
+	return (nodes);
 }
